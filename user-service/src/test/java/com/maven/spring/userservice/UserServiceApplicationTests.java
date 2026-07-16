350eee5b96a5aeeae4ca76c3fb979ef38f9109a1
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-07-16 21:07:52 +0530

    initial-commit

---
+++ user-service/src/test/java/com/maven/spring/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.spring.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
